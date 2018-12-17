3b9f7a7f89595210da8805e7209b9e6ef97e3423
vandanamandlik <vandana_mandlik@persistent.com>
2018-12-17 19:25:32 +0530

    Modified periodic_snapshotter.cpp file as per ros2 migration guide. Only porting is done but not tested.

--- examples/periodic_snapshotter.cpp
+++ examples/periodic_snapshotter.cpp
@@ -33,15 +33,25 @@
 *********************************************************************/
 
 #include <cstdio>
-#include <ros/ros.h>
-
+#include "rclcpp/rclcpp.hpp"
+#include <string>
+#include <chrono>
 // Services
-#include "laser_assembler/AssembleScans.h"
+#include "laser_assembler_srv_gen/srv/assemble_scans.hpp"
 
 // Messages
-#include "sensor_msgs/PointCloud.h"
+#include "sensor_msgs/msg/point_cloud.hpp"
+#include "rcutils/cmdline_parser.h"
+#include "rclcpp/clock.hpp"
+#include "std_msgs/msg/string.hpp"
 
+#define ROS_ERROR printf
+#define ROS_INFO printf
+#define ROS_WARN printf
+#define ROS_DEBUG printf
 
+using namespace std::chrono_literals;
+using namespace std::placeholders;
 /***
  * This a simple test app that requests a point cloud from the
  * point_cloud_assembler every 4 seconds, and then publishes the
@@ -50,59 +60,95 @@
 namespace laser_assembler
 {
 
-class PeriodicSnapshotter
+class PeriodicSnapshotter: public rclcpp::Node
 {
 
 public:
 
-  PeriodicSnapshotter()
+  explicit PeriodicSnapshotter(const std::string & service_name)
+  : Node(service_name)
   {
-    // Create a publisher for the clouds that we assemble
-    pub_ = n_.advertise<sensor_msgs::PointCloud> ("assembled_cloud", 1);
+    std::cerr<< " Inside PeriodicSnapshotter constructor " << "\n";
 
-    // Create the service client for calling the assembler
-    client_ = n_.serviceClient<AssembleScans>("assemble_scans");
+    client_ = this->create_client<laser_assembler_srv_gen::srv::AssembleScans>("build_cloud");
+    
+    if (!client_->wait_for_service(20s)) {
+      printf("Service not available after waiting");
+      return;
+    }
+    printf(" Service assemble_scans started successfully");
+    //ROS_INFO("Service [%s] detected", SERVICE_NAME.c_str());
+    
+    // Create a function for when messages are to be sent.
+    auto timer_callback = [this]() -> void {
+        // msg_->name = "Hello World: " + std::to_string(count_++);
+        // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg_->status)
+        //RCLCPP_INFO(this->get_logger(), "Publishing: namespce %s and name is %s ",
+         // this->get_namespace(), this->get_name());
 
-    // Start the timer that will trigger the processing loop (timerCallback)
-    timer_ = n_.createTimer(ros::Duration(5,0), &PeriodicSnapshotter::timerCallback, this);
+        // Put the message into a queue to be processed by the middleware.
+        // This call is non-blocking.
+        this->timerCallback();
+      };
+      
+    // Create a publisher with a custom Quality of Service profile.
+    rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
+    custom_qos_profile.depth = 7;
+    pub_ = this->create_publisher<sensor_msgs::msg::PointCloud>(
+      "/assembled_cloud", custom_qos_profile);
+
+    // Use a timer to schedule periodic message publishing.
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(25), timer_callback);
 
     // Need to track if we've called the timerCallback at least once
     first_time_ = true;
   }
 
-  void timerCallback(const ros::TimerEvent& e)
+  //void timerCallback(const builtin_interfaces::msg::Time& e)
+  void timerCallback()
   {
 
     // We don't want to build a cloud the first callback, since we we
     //   don't have a start and end time yet
     if (first_time_)
     {
+      last_time = rclcpp::Clock().now();
       first_time_ = false;
       return;
     }
 
     // Populate our service request based on our timer callback times
-    AssembleScans srv;
-    srv.request.begin = e.last_real;
-    srv.request.end   = e.current_real;
-
-    // Make the service call
-    if (client_.call(srv))
-    {
-      ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.points.size())) ;
-      pub_.publish(srv.response.cloud);
-    }
-    else
-    {
-      ROS_ERROR("Error making service call\n") ;
-    }
-  }
+    auto request = std::make_shared<laser_assembler_srv_gen::srv::AssembleScans::Request>();
+    
+    request->begin = last_time.sec;
+    last_time = rclcpp::Clock().now();
+    request->end = last_time.sec;
+      
+      
+    using ServiceResponseFuture =
+        rclcpp::Client<laser_assembler_srv_gen::srv::AssembleScans>::SharedFuture;
+      
+      auto response_received_callback = [this](ServiceResponseFuture future) {
+        auto result = future.get();
+        printf("\n                                      Cloud points size = %ld\n", result.get()->cloud.points.size());
+        //RCLCPP_INFO(this->get_logger(), "Got result: [%" PRId64 "]", future.get()->sum)
+        if (result.get()->cloud.points.size() > 0 ) {
+          
+          result.get()->cloud;
+          pub_->publish(result.get()->cloud);
+        }
+        else
+          ROS_INFO("Got an empty cloud. Going to try again on the next scan");
+      };
+      
+    auto result = client_->async_send_request(request);
+}
 
 private:
-  ros::NodeHandle n_;
-  ros::Publisher pub_;
-  ros::ServiceClient client_;
-  ros::Timer timer_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud>::SharedPtr pub_;
+  rclcpp::Client<laser_assembler_srv_gen::srv::AssembleScans>::SharedPtr client_;
+  builtin_interfaces::msg::Time last_time;
+  rclcpp::TimerBase::SharedPtr timer_;
   bool first_time_;
 } ;
 
@@ -112,12 +158,13 @@ using namespace laser_assembler ;
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "periodic_snapshotter");
-  ros::NodeHandle n;
-  ROS_INFO("Waiting for [build_cloud] to be advertised");
-  ros::service::waitForService("build_cloud");
-  ROS_INFO("Found build_cloud! Starting the snapshotter");
-  PeriodicSnapshotter snapshotter;
-  ros::spin();
+  rclcpp::init(argc, argv);
+  auto service_name = std::string("periodic_snapshotter");
+  char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-s");
+  if (nullptr != cli_option) {
+    service_name = std::string(cli_option);
+  }
+  auto node = std::make_shared<PeriodicSnapshotter>(service_name);
+  rclcpp::spin(node);
   return 0;
 }
